0ec6303df7d590791dfe00fc38bdc4752b20ee69
hamidazim321 <129533107+hamidazim321@users.noreply.github.com>
2023-08-01 12:59:24 +0500

    Add tests for the comments Counter Function

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  mode: 'development',
+  mode: 'production',
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
